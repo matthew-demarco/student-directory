c978262289ff422f3524d51f9070b373d8cb85b0
Matthew DeMarco <matthewd0815@gmail.com>
2026-08-07 19:17:26 -0400

    Add student update functionality

--- app.py
+++ app.py
@@ -61,6 +61,37 @@ def delete_student(student_id):
 
     return redirect("/")
 
+@app.route("/edit/<int:student_id>")
+def edit_student(student_id):
+    connection = get_connection()
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+	    "SELECT id, name, school FROM students WHERE id = %s;",
+	    (student_id,),
+
+        )
+        student = cursor.fetchone()
+
+    connection.close()
+    return render_template("edit.html", student=student)
+
+@app.route("/update/<int:student_id>", methods=["POST"])
+def update_student(student_id):
+    name = request.form["name"]
+    school = request.form["school"]
+
+    connection = get_connection()
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+	    "UPDATE students SET name = %s, school = %s WHERE id = %s",
+	    (name, school, student_id),
+	)
+    connection.commit()
+    connection.close()
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
